2aaec5830640df4d2f9c475ff72662970a4eed34
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-08-23 13:37:11 +1000

    stopped cpp new class buffer opening

--- lua/config/cpphelper.lua
+++ lua/config/cpphelper.lua
@@ -1,44 +1,49 @@
 -- Function to generate C++ class
+-- Function to generate C++ class
 function generateCppClass(className, classPath)
     -- Editing header file
     local header = classPath .. "/" .. className .. ".h"
-    vim.cmd("silent e " .. header)
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, {
-        "#pragma once",
-        " ",
-        "class " .. className,
-        "{",
-        "   public:",
-        "      " .. className .. "();",
-        "      virtual ~" .. className .. "();",
-        "   protected:",
-        "   private:",
-        "};",
-    })
-    vim.cmd("write")
+    local headerFile = assert(io.open(header, "w"))
+    local headerContent =
+        [[
+   #pragma once
+
+   class ]] .. className .. [[
+   {
+   public:
+      ]] .. className .. [[();
+      virtual ~]] .. className .. [[();
+   protected:
+   private:
+   };
+  ]]
+    headerFile:write(headerContent)
+    headerFile:close()
 
     -- Editing source file
     local src = classPath .. "/" .. className .. ".cpp"
-    vim.cmd("silent e " .. src)
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, {
-        "#include \"" .. className .. ".h\"",
-        " ",
-        className .. "::" .. className .. "()",
-        "{",
-        " ",
-        "}",
-        " ",
-        " ",
-        className .. "::~" .. className .. "()",
-        "{",
-        " ",
-        "}",
-    })
-    vim.cmd("write")
-end
+    local srcFile = assert(io.open(src, "w"))
+    local srcContent =
+        [[
+   #include "]] .. className .. [[.h"
 
--- Keybinding to generate C++ class in the current nvim-tree directory
-vim.api.nvim_set_keymap('n', '<S-c>', ':lua generateCppClass(vim.fn.input("Enter Class Name > "), vim.fn.expand("%:p:h"))<CR>', { noremap = true, silent = true })
+   ]] .. className .. [[::]] .. className .. [[()
+   {
+   }
+
+   ]] .. className .. [[::~]] .. className .. [[()
+   {
+   }
+  ]]
+    srcFile:write(srcContent)
+    srcFile:close()
+end -- Keybinding to generate C++ class in the current nvim-tree directory
+
+vim.api.nvim_set_keymap('n', '<S-c>',
+    ':lua generateCppClass(vim.fn.input("Enter Class Name > "), vim.fn.expand("%:p:h"))<CR>',
+    { noremap = true, silent = true })
 
 
-vim.api.nvim_set_keymap('n', '<leader>cc', ':lua generateCppClass(vim.fn.input("Enter Class Name > "), vim.fn.expand("%:p:h"))<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>cc',
+    ':lua generateCppClass(vim.fn.input("Enter Class Name > "), vim.fn.expand("%:p:h"))<CR>',
+    { noremap = true, silent = true })
