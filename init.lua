26f79f935530cb073cf29e7aca5975dc3beae1a8
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-08-25 22:38:25 +1000

    changed line number colors because hard to read/rel jump to

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ require "config.remap"
 require "config.packer"
 require "config.cpphelper"
 require "config.bwpost"
+
+--I was having some problem reading the line numbers
+vim.api.nvim_set_hl(0, 'LineNr', { fg = "lightgray"})
+vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "lightgray"})
