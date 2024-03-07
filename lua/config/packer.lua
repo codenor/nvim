8b18b51a096253994faf2ddaebd7a211c07a38f6
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-03-07 11:41:42 +1100

    added commentary

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -21,6 +21,7 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use 'folke/tokyonight.nvim'
+    use 'tpope/vim-commentary'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
